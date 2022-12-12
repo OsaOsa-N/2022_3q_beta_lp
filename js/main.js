14435ee176cc20eae3d44a8441943fb5d975937f
OsaOsa-N <haruya_20n1103814@nnn.ed.jp>
2022-12-12 09:19:15 +0900

    リンクごまかし

--- js/main.js
+++ js/main.js
@@ -6,3 +6,23 @@ dropdown.firstElementChild.addEventListener("click",()=>{
 window.addEventListener("resize", ()=>{
   dropdown.classList.remove("open");
 })
+
+const a = document.querySelectorAll("a");
+a.forEach(
+  (v)=>{
+    v.addEventListener("click", function (e) {
+      e.preventDefault();
+      alert("遷移先が存在しません");
+    });
+  }
+);
+
+const b = document.querySelectorAll("button");
+b.forEach(
+  (v)=>{
+    v.addEventListener("click", function (e) {
+      e.preventDefault();
+      alert("遷移先が存在しません");
+    });
+  }
+);
